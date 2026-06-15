4f17eabc8758fb71b56e0a0e2d297694cc7cd951
RupeshKumaranKR <rupeshkumarankr@gmail.com>
2026-06-15 10:08:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (17.73%)

---
+++ 0263-ugly-number/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n==0:
+            return False
+        while(1):
+            if n%2==0:
+                n=n//2
+            elif n%3==0:
+                n=n//3
+            elif n%5==0:
+                n=n//5
+            elif n!=1:
+                return False
+            elif n==1:
+                return True
+
