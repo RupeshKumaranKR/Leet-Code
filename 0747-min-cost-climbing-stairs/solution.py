6163356c1959dd7da800a9b2a14f2ad94780d735
RupeshKumaranKR <rupeshkumarankr@gmail.com>
2026-06-18 10:09:02 +0000

    Sync LeetCode submission Runtime - 10 ms (12.07%), Memory - 14.5 MB (6.50%)

---
+++ 0747-min-cost-climbing-stairs/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        memo = {}
+        
+        def dp(i):
+            if i >= len(cost):
+                return 0
+            
+            if i in memo:
+                return memo[i]
+            
+            
+            memo[i] = cost[i] + min(dp(i + 1), dp(i + 2))
+            
+            return memo[i]
+            
+        return min(dp(0), dp(1))
