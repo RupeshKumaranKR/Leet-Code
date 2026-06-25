b68acab1529c5cf85e0b2671997e25edef603e8f
RupeshKumaranKR <rupeshkumarankr@gmail.com>
2026-06-25 16:15:04 +0530

    Time: 0 ms (100%), Space: 12.9 MB (64.82%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left=0
+        right=len(nums)-1
+        while(left<=right):
+            m=(left+right)//2
+            if nums[m]==target:
+                return m
+            if target>nums[m]:
+                left=m+1
+            else:
+                right=m-1
+        return left
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
